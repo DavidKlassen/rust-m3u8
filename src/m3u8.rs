84c35975b312e54198654cff80899ee0dc85ed21
David Klassen <f0rk.tt@gmail.com>
2015-06-24 19:02:11 +0200

    Defining data structs WIP

--- src/m3u8.rs
+++ src/m3u8.rs
@@ -1,32 +1,25 @@
-
-#[derive(Debug)]
-pub struct DecoderError(String);
-#[derive(Debug)]
-pub struct EncoderError(String);
-
-/// Decodes a m3u8 `&str` playlist into a Playlist struct.
-pub fn decode(s: &str) -> Result<Playlist, DecoderError> {
-    let mut pls = Playlist::new();
-    pls.tracks = s.split('\n').collect::<Vec<_>>();
-
-    Ok(pls)
+pub struct MediaPlaylist {
+    segment_duration: i32,
+    segments: Vec<MediaSegment>
 }
 
-/// Encodes a Playlist into a m3u8 `String` playlist.
-pub fn encode(playlist: &Playlist) -> Result<String, EncoderError> {
-    Ok(playlist.tracks.connect("\n"))
+pub struct MasterPlaylist {
+    variants: Vec<Variant>
 }
 
-pub struct Playlist<'a> {
-    tracks: Vec<&'a str>
+pub struct MediaSegment {
+    uri: String,
+    byte_range: String,
+    sequence_number: i32
 }
 
-impl<'a> Playlist<'a> {
-    pub fn new() -> Playlist<'a> {
-        Playlist {
-            tracks: vec![]
-        }
-    }
+pub struct Rendition {
+    uri: String
+}
+
+pub struct Variant {
+    renditions: Vec<Rendition>,
+    playlist: MediaPlaylist
 }
 
 #[cfg(test)]
@@ -34,17 +27,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_decode() {
-        let playlist = decode("foo\nbar").unwrap();
-        assert_eq!(playlist.tracks.len(), 2);
-    }
-
-    #[test]
-    fn test_encode() {
-        let mut playlist = Playlist::new();
-        playlist.tracks.push("foo");
-        playlist.tracks.push("bar");
-        let m3u8 = encode(&playlist).unwrap();
-        assert_eq!(&m3u8[..], "foo\nbar");
+    fn test_stub() {
+        assert_eq!(2, 2);
     }
 }
