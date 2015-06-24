41016dcc1067fbe7402b26b97b506adfa43fd681
David Klassen <f0rk.tt@gmail.com>
2015-06-24 18:18:40 +0200

    Added tracks

--- src/m3u8.rs
+++ src/m3u8.rs
@@ -6,22 +6,25 @@ pub struct EncoderError(String);
 
 /// Decodes a m3u8 `&str` playlist into a Playlist struct.
 pub fn decode(s: &str) -> Result<Playlist, DecoderError> {
-    Ok(Playlist::new(s))
+    let mut pls = Playlist::new();
+    pls.tracks = s.split('\n').collect::<Vec<_>>();
+
+    Ok(pls)
 }
 
 /// Encodes a Playlist into a m3u8 `String` playlist.
 pub fn encode(playlist: &Playlist) -> Result<String, EncoderError> {
-    Ok(playlist.name.clone())
+    Ok(playlist.tracks.connect("\n"))
 }
 
-pub struct Playlist {
-    name: String
+pub struct Playlist<'a> {
+    tracks: Vec<&'a str>
 }
 
-impl Playlist {
-    pub fn new(name: &str) -> Playlist {
+impl<'a> Playlist<'a> {
+    pub fn new() -> Playlist<'a> {
         Playlist {
-            name: name.to_string()
+            tracks: vec![]
         }
     }
 }
@@ -32,13 +35,16 @@ mod test {
 
     #[test]
     fn test_decode() {
-        let playlist = decode("ololo").unwrap();
-        assert_eq!(&playlist.name[..], "ololo");
+        let playlist = decode("foo\nbar").unwrap();
+        assert_eq!(playlist.tracks.len(), 2);
     }
 
     #[test]
     fn test_encode() {
-        let playlist = encode(&Playlist::new("test")).unwrap();
-        assert_eq!(&playlist[..], "test");
+        let mut playlist = Playlist::new();
+        playlist.tracks.push("foo");
+        playlist.tracks.push("bar");
+        let m3u8 = encode(&playlist).unwrap();
+        assert_eq!(&m3u8[..], "foo\nbar");
     }
 }
